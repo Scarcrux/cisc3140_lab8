9024eeb925d1e1e1cf97597d947f2fe08a596554
Scarcrux <reg@scarcrux.com>
2020-10-25 01:10:58 -0400

    Update palindrome.scm

--- palindrome.scm
+++ palindrome.scm
@@ -5,8 +5,8 @@
   (let loop ((str (string->list str))
 ; reverse string in list form
              (revstr (reverse (string->list str))))
-;check if string is empty
+; check if string is empty
     (or (null? str)
-;compare lists for equivalency
+; compare lists for equivalency
         (and (char=? (car str) (car revstr))
              (loop (cdr str) (cdr revstr))))))
